294cc12f54a983a820924217cbc95d34538cd9e4
Seva D <ukwahlula@gmail.com>
2021-12-06 01:19:30 +0300

    Added ci

---
+++ .github/pull_request_template.md
@@ -0,0 +1,8 @@
+### References
+
+<!-- (Which ticket does the merge request reference e.g STORY-BUG-123) -->
+
+### Summary
+
+<!-- (What does the merge request solve?) -->
+<!-- For a bug: (Which bug does the merge request solve?) -->
--- api/settings.py
+++ api/settings.py
@@ -1,10 +1,20 @@
+import os
+from environs import Env
+
+# BASE PATH
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+env = Env()
+ENV_FILE = env.str("ENV_FILE", default=".env")
+env.read_env(os.path.join(BASE_DIR, ENV_FILE))
+
 # ENV
-TEST_ENV = True
+TEST_ENV = env.bool("TEST_ENV", False)
 
 # DB
-DATABASE_URI = ""
+DATABASE_URI = env.str("DATABASE_URI", None)
 
 # JWT
-JWT_SECRET_KEY = "secret"
-JWT_ACCESS_TOKEN_EXPIRED_AT = 36000  # in sec
-JWT_REFRESH_TOKEN_EXPIRED_AT = 360000  # in sec
+JWT_SECRET_KEY = env.str("JWT_SECRET_KEY")
+JWT_ACCESS_TOKEN_EXPIRED_AT = env.int("JWT_ACCESS_TOKEN_EXPIRED_AT")  # in sec
+JWT_REFRESH_TOKEN_EXPIRED_AT = env.int("JWT_REFRESH_TOKEN_EXPIRED_AT")   # in sec
